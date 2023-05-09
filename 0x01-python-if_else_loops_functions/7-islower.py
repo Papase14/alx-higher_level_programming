3c2feb212b20b2367dd08a166fa4e24537125e00
Tshupane Morake <tshupane@outlook.com>
2023-05-10 00:49:24 +0200

    Function to detect lowercase values

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 
 def islower(c):
     for i in range(97, 123):
-        if ord(c) >= i or ord(c) <= i:
+        if ord(c) >= i and ord(c) <= i:
             return True
         else:
             return False
