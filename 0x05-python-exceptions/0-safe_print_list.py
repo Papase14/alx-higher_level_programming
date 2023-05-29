70d69653acb13ad49ea4423f5067fa4fdf92aa06
Tshupane Morake <tshupane@outlook.com>
2023-05-29 20:21:43 +0200

    Safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pithon3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     
@@ -11,3 +11,12 @@ def safe_print_list(my_list=[], x=0):
 
     print()
     return count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
