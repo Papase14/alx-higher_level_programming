5a9b8063e0e67a55b33a327b240d164c4f38de56
Tshupane Morake <tshupane@outlook.com>
2023-06-04 23:55:54 +0200

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
+"""
+    0-add_integer: add_integer()
+"""
+
 
 def add_integer(a, b=98):
+    """
+        add_integer returns the sum of two integers
+        Args:
+            a: first number.
+            b: second number.
+        Returns:
+            sum of the two integers
+    """
     if (type(a) != float) and (type(a) != int):
         raise TypeError("a must be an integer")
     elif (type(b) != float) and (type(b) != int):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
-
-
-
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
