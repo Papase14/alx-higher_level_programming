4ecf618f373176a17c4d68bfecaef810c6e1ea4c
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-06-13 00:32:13 +0200

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    5-base_geometry: class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        BaseGeometry
+        Methods:
+            area() - raises an Exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
