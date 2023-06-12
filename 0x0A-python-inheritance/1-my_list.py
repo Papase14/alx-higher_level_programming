c6c6422198c6344b96f9e2de580f87206e4773d8
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-06-13 00:22:26 +0200

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    1-my_list: class MyList
+"""
+
+
+class MyList(list):
+    """
+        This class inherits from list.
+        Methods:
+            print_sorted - prints the list in ascending order
+    """
+    def print_sorted(self):
+        """
+           prints a list in ascending order.
+        """
+        print(sorted(self))
