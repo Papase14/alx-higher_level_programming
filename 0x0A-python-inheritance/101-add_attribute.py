03e7232a9199b5e8191a3a608d69816047f2bfed
Tshupane Morake <40323964+Papase14@users.noreply.github.com>
2023-06-13 01:10:30 +0200

    13. Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,6 +8,6 @@ def add_attribute(cls, name, value):
     """
         adds a new attribute if possible.
     """
-    if hasattr(cls, "__dict__") == False:
+    if hasattr(cls, "__dict__") is False:
         raise TypeError("can't add new attribute")
     setattr(cls, name, value)
