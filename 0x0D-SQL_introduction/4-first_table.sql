c785685308a6b142c6dd15733dfa29964359e8f8
Tshupane Morake <tshupane@outlook.com>
2023-07-19 00:57:04 +0200

    4. First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- SCRIPT THAT CREATES A TABLE IN THE CURRENT DATABASE
 -- cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 
-CREATE TABLE IF NOT EXIST first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
