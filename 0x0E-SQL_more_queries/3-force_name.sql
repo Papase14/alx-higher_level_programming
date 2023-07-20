19cdf57936cda02114781ca538b682fc559dac20
Tshupane Morake <tshupane@outlook.com>
2023-07-21 00:39:29 +0200

    First

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,11 @@
+-- creates the table force_name
+-- force_name description:
+-- id INT
+-- name VARCHAR(256) can’t be null
+-- cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+
+CREATE TABLE IF NOT EXISTS force_name
+(
+	id INT,
+	name VARCHAR(256) NOT NULL
+);
