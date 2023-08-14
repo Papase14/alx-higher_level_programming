47736b1cff79c3afff52cc118abbd1ec92bc52c4
Tshupane Morake <tshupane@outlook.com>
2023-08-14 20:48:09 +0200

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
-const parameter = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-parameter.forEach((element) => console.log(element));
+const lines = [
+    "C is fun",
+    "Python is cool",
+    "JavaScript is amazing"
+];
+  
+for (const line of lines) {
+    console.log(line);
+}
