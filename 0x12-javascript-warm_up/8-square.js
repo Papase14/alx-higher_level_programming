2795812165599940880964452662e5c4f1260c7a
Tshupane Morake <tshupane@outlook.com>
2023-08-14 21:10:39 +0200

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
-const { argv } = require('process');
-const size = parseInt(argv[2]);
+const size = parseInt(process.argv[2]);
 
-const printSquare = (size) => {
-  const row = 'X'.repeat(size);
-  for (let i = 0; i < size; i++) console.log(row);
-};
-
-Number.isInteger(size) ? printSquare(size) : console.log('Missing size');
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  if (size > 0) {
+    for (let i = 0; i < size; i++) {
+      console.log('X'.repeat(size));
+    }
+  }
+}
