8fb790ee94ca7503f83ff69ae1dd3a89bce1de4f
Tshupane Morake <tshupane@outlook.com>
2023-08-21 04:14:28 +0200

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Lists all states with a name starting with
-N (upper N) from the database hbtn_0e_0_usa
+N(upper N) from the database hbtn_0e_0_usa
 """
 
 if __name__ == '__main__':
