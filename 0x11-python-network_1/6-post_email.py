a37b249f37926de64779ad0f0f6103918c3e23d1
Tshupane Morake <tshupane@outlook.com>
2023-09-01 22:05:42 +0200

    6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: ./6-post_email.py <URL> <email>")
+    sys.exit(1)
+
+url = sys.argv[1]
+email = sys.argv[2]
+data = {"email": email}
+
+try:
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+    print("Your email is:", response.text)
+
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
