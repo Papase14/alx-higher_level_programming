9bcdf9123489cedd46f490ab320e67c6e141c794
Tshupane Morake <tshupane@outlook.com>
2023-09-26 21:03:41 +0200

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 3) {
-  console.log('Usage: node read_file.js <file_path>');
-  process.exit(1);
-}
-
-// Get the file path from the command line arguments
-const filePath = process.argv[2];
-
-// Read and print the content of the file
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(`Error reading file: ${filePath}`);
-    console.error(err);
-  } else {
-    console.log(data);
-  }
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+    function (err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(data);
+    });
