e83fca34ee1082d0e007a8b499a99fe0abb63089
Tshupane Morake <tshupane@outlook.com>
2023-09-26 21:19:03 +0200

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const filesys = require('fs')
+const filesys = require('fs');
 filesys.readFile(process.argv[2], 'utf-8',
   function (err, data) {
     if (err) {
-      console.log(err)
-      return
+      console.log(err);
+      return;
     }
-    console.log(data)
-  })
+    console.log(data);
+  });
