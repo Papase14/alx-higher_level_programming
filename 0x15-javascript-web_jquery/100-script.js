2ddb4b72557146298491a04159c92ff3314ef7a3
Tshupane Morake <tshupane@outlook.com>
2023-10-04 14:34:34 +0200

    10. No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+document.addEventListener("DOMContentLoaded", function () {
+    const headerSelect = document.querySelector("header");
+    headerSelect.style.color = "FF0000";
+});
