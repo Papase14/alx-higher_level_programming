56fc029438e0c365b0f039e61f703ca9ecae0fa5
Tshupane Morake <tshupane@outlook.com>
2023-10-04 14:32:47 +0200

    9. Say Hello!

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,12 @@
-# 
+#!/usr/bin/node
+
+$(document).ready(function () {
+    $.ajax({
+        url: "https://fourtonfish.com/hellosalut/?lang=fr",
+        type: "GET",
+        datatype: "json",
+    })
+        .done(function (json) {
+            $("DIV#hello").text(json.hello);
+        })
+});
