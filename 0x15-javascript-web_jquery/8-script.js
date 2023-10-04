5101c6d7d1c0db4c7bb0aafe7be0eb66c6082ab5
Tshupane Morake <tshupane@outlook.com>
2023-10-04 14:30:20 +0200

    8. Star Wars movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,9 @@
-# 
+#!/usr/bin/node
+
+let url = 'https://swapi.co/api/films/?format=json';
+$.get(url, function (data) {
+    let films = data.results;
+    for (let film of films) {
+        $('ul#list_movies').append(`<li>${film.title}</li>`);
+    }
+});
