003ca90950649b06448b830a09f8866679c6f207
Tshupane Morake <tshupane@outlook.com>
2023-10-04 14:29:35 +0200

    7. Star wars character

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
+const { get } = require("request");
+
 let url = 'https://swapi.co/api/people/5/?format=json';
-$.get(url, function (data, stat) {
+$.get(url, function (data) {
     $('div#character').text(data.name);
 });
