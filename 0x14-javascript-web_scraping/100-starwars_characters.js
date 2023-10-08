98e8bf100f910e367ff78dfccb4cf80d0ae2bec0
Tshupane Morake <tshupane@outlook.com>
2023-10-09 00:50:10 +0200

    Update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,18 +4,18 @@ const req = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
 req.get(url + id, function (error, res, body) {
-    if (error) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const filmcast = data.characters;
+  for (const i of filmcast) {
+    req.get(i, function (error, res, body1) {
+      if (error) {
         console.log(error);
-    }
-    const data = JSON.parse(body);
-    const filmcast = data.characters;
-    for (const i of filmcast) {
-        req.get(i, function (error, res, body1) {
-            if (error) {
-                console.log(error);
-            }
-            const castmember = JSON.parse(body1);
-            console.log(castmember.name);
-        });
-    }
+      }
+      const castmember = JSON.parse(body1);
+      console.log(castmember.name);
+    });
+  }
 });
