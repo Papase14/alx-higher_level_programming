efcf42e419812ecc07357e6b37e298c5d3f363a9
Tshupane Morake <tshupane@outlook.com>
2023-10-09 00:47:29 +0200

    Update

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,4 @@
 #!/usr/bin/node
-
-// Number of films with the given character ID
-
+const fs = require('fs');
 const request = require('request');
-let num = 0;
-
-request.get(process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    content.results.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character.includes(18)) {
-          num += 1;
-        }
-      });
-    });
-    console.log(num);
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
